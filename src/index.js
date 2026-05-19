faaf1d1258ec61bda4dbd98b01d53ee669dea563
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-05-19 16:56:28 +0800

    Full portfolio built

---
+++ src/index.js
@@ -0,0 +1,27 @@
+function validateForm() {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const form = document.getElementById("Form");
+    const errorElement = document.getElementById("error");
+
+    form.addEventListener("submit", (e)=> {
+        let messages =[];
+        if (name.value === " " || name.value == null) {
+            messages.push("Name is required");
+        }
+
+        if (email.value === " " || email.value == null) {
+            messages.push("Email is required");
+        }
+
+        if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(", ");
+    }
+        
+    });
+
+
+}
+
+validateForm();
