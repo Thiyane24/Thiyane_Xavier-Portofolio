e42e170505245800e042b4f9914be49ae943f62f
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-05-19 17:38:32 +0800

    tech stack: Docker, Airflow, Power BI, Python

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 function validateForm() {
     const name = document.getElementById("name");
     const email = document.getElementById("email");
-    const form = document.getElementById("Form");
+    const form = document.querySelector("form");
     const errorElement = document.getElementById("error");
 
-    form.addEventListener("submit", (e)=> {
-        let messages =[];
-        if (name.value === " " || name.value == null) {
+    form.addEventListener("submit", (e) => {
+        let messages = [];
+
+        if (!name.value || name.value.trim() === "") {
             messages.push("Name is required");
         }
 
-        if (email.value === " " || email.value == null) {
+        if (!email.value || email.value.trim() === "") {
             messages.push("Email is required");
         }
 
         if (messages.length > 0) {
-        e.preventDefault();
-        errorElement.innerText = messages.join(", ");
-    }
-        
+            e.preventDefault();
+            errorElement.innerText = messages.join(", ");
+        } else {
+            errorElement.innerText = "";
+        }
     });
-
-
 }
 
 validateForm();
